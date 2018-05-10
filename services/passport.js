17f7a7b4b3a7cdc9f43679e4fcfb66f053235974
root <root@DESKTOP-GNBQSCC.localdomain>
2018-05-10 23:06:29 +0200

    video 65

--- services/passport.js
+++ services/passport.js
@@ -20,23 +20,17 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-      proxy: true
+      callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then(existingUser => {
-          if (existingUser) {
-            // we already have a record for this profile ID
-            done(null, existingUser);
-          } else {
-            // new user with this profile ID
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        })
-        .catch(err => console.log("[ERROR] : " + err));
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
